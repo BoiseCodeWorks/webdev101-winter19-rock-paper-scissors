c591452b0850f749a31c77e3b01a750a31ce1964
Darryl Kilzer <darrylkilzer@gmail.com>
2019-11-11 19:35:55 -0800

    JJ

--- app.js
+++ app.js
@@ -1,60 +1,106 @@
-let compChoice 
+let compChoice
+let playerChoice
 let resultElem = document.querySelector("#result")
-let choices = ["rock", "paper", "scissors"]
 let compChoiceElem = document.querySelector("#compChoiceText")
+let rockWinElem = document.querySelector("#rock-win")
+let paperWinElem = document.querySelector("#paper-win")
+let scissorWinElem = document.querySelector("#scissors-win")
 
-function play(playerChoice) {
-compChoice = choices[Math.floor( Math.random() *3 )]
-console.log(playerChoice, compChoice)
-compChoiceElem.textContent = compChoice
-  switch (playerChoice) {
-    case "rock":
-      rock()
-      break
-    case "paper":
-      paper()
-      break
-    case "scissors":
-      scissors()
-      break
-    default: console.error("error")
+
+let choices = [
+  {
+    name: "rock",
+    beats: "scissors",
+    loses: "paper",
+    winCount: 0
+  },
+  {
+    name: "paper",
+    beats: "rock",
+    loses: "scissors",
+    winCount: 0
+  },
+  {
+    name: "scissors",
+    beats: "paper",
+    loses: "rock",
+    winCount: 0
+  }]
+
+function play(playerChoiceIndex) {
+  playerChoice = choices[playerChoiceIndex]
+  compChoice = choices[Math.floor(Math.random() * 3)]
+  console.log(playerChoice, compChoice)
+  compChoiceElem.textContent = compChoice.name
+
+  if (compChoice.name == playerChoice.beats) {
+    changeWinCount(playerChoice)
+    resultElem.textContent = "You win!"
+  } else if (compChoice.name == playerChoice.loses) {
+    resultElem.textContent = "You lose!"
+  } else {
+    resultElem.textContent = "You tied!"
   }
 }
 
-function rock() {
-  if (compChoice == "rock") {
-    resultElem.textContent = "tie"
-  }
-  else if (compChoice == "paper") {
-    resultElem.textContent = "lose"
-  }
-  else {
-    resultElem.textContent = "win"
+function changeWinCount(playerChoice) {
+  playerChoice.winCount++
+  if (playerChoice.name == "rock") {
+    rockWinElem.textContent = playerChoice.winCount
+  } else if (playerChoice.name == "paper") {
+    paperWinElem.textContent = playerChoice.winCount
+  } else {
+    scissorWinElem.textContent = playerChoice.winCount
   }
 }
 
-  function paper() {
-    if (compChoice == "rock") {
-      resultElem.textContent = "win"
-    }
-    else if (compChoice == "paper") {
-      resultElem.textContent = "tie"
-    }
-    else {
-      resultElem.textContent = "lose"
-    }
-  }
+// switch (playerChoice.name) {
+//   case "rock":
+//     rock()
+//     break
+//   case "paper":
+//     paper()
+//     break
+//   case "scissors":
+//     scissors()
+//     break
+//   default: console.error("error")
+// }
+// }
 
-    function scissors() {
-      if (compChoice == "rock") {
-        resultElem.textContent = "lose"
-      }
-      else if (compChoice == "paper") {
-        resultElem.textContent = "win"
-      }
-      else {
-        resultElem.textContent = "tie"
-      }
+// function rock() {
+//   if (compChoice.name == playerChoice.beats) {
+//     resultElem.textContent = "You win!"
+//   }
+//   else if (compChoice == "paper") {
+//     resultElem.textContent = "lose"
+//   }
+//   else {
+//     resultElem.textContent = "win"
+//   }
+// }
 
-    }
-    
+// function paper() {
+//   if (compChoice == "rock") {
+//     resultElem.textContent = "win"
+//   }
+//   else if (compChoice == "paper") {
+//     resultElem.textContent = "tie"
+//   }
+//   else {
+//     resultElem.textContent = "lose"
+//   }
+// }
+
+// function scissors() {
+//   if (compChoice == "rock") {
+//     resultElem.textContent = "lose"
+//   }
+//   else if (compChoice == "paper") {
+//     resultElem.textContent = "win"
+//   }
+//   else {
+//     resultElem.textContent = "tie"
+//   }
+
+// }
